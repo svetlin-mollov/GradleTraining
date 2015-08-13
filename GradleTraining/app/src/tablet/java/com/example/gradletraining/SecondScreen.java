bc2d8321d75539d753ce5a89de0756b2fe7f85a8
svetlin-mollov <ssmollov@uni-sofia.bg>
2015-08-13 08:33:33 +0300

    Added different second screen for tablet.

--- GradleTraining/app/src/tablet/java/com/example/gradletraining/SecondScreen.java
+++ GradleTraining/app/src/tablet/java/com/example/gradletraining/SecondScreen.java
@@ -1,13 +1,43 @@
 package com.example.gradletraining;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SecondScreen extends ActionBarActivity {
+import com.example.gradletraining.activity.BaseActivity;
+import com.example.gradletraining.fragment.BrowserFragment;
+
+public class SecondScreen extends BaseActivity {
+
+    private BrowserFragment mBrowserFragment;
+    private EditText mUrlEditText;
+    private Button mGoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+
+        // Load browser fragment
+        mBrowserFragment = new BrowserFragment();
+        replaceFragment(mBrowserFragment, R.id.container_content);
+
+        mUrlEditText = (EditText) findViewById(R.id.edittext_url);
+        mGoButton = (Button) findViewById(R.id.button_go);
+        mGoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                loadUrl(mUrlEditText.getText().toString());
+            }
+        });
+
+        loadUrl(Constants.BASE_URL);
+    }
+
+    private void loadUrl(String url) {
+        mUrlEditText.setText(url);
+        mBrowserFragment.loadUrl(url);
     }
 }
